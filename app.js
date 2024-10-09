66d48ff24372292761cd1e1ceff79df100810125
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2024-10-08 22:15:43 -0300

    Adicionando as funcionalidades na API

--- app.js
+++ app.js
@@ -1,40 +1,66 @@
 const express = require('express')
+const produtoService = require('./service/produto_service')
+
 const app = express()
 const PORT = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+//API para testar se a URL está no ar (http://localhost:3000)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//Listar Produtos
 app.get('/produtos', (req, res)=> {
-    res.json({msg: "Listando produtos!"})
+    res.json(produtoService.listar())
 })
 
+//Buscar por id
 app.get('/produtos/:id', (req, res) => {
-  const id = req.params.id;
-  res.json({msg: "Buscando produto pelo id " + id});
+  // O + antes converte o valor para number (na URL vem como string)
+  const id = +req.params.id;
+  try {
+    res.json(produtoService.buscarPorId(id));
+  } catch(err) {
+    res.status(err.id).json(err)
+  }
 })
 
+//Inserir
 app.post('/produtos', (req, res)=> {
     const produto = req.body;
-    console.log(produto);
-    res.status(201).json({msg:"Inserindo produto.."})
+    try{
+      const produtoInserido = produtoService.inserir(produto);
+      res.status(201).json(produtoInserido)
+    }
+    catch(err){
+      res.status(err.id).json(err)
+    }
 })
 
+//Atualizar
 app.put('/produtos/:id', (req, res) => {
-  const id = req.params.id;
+  const id = +req.params.id;
   const produto = req.body;
-  console.log(produto);
-
-  res.json({msg:"Atualizando produto com id "+id});
+  try{
+    const produtoAtualizado = produtoService.atualizar(id, produto);
+    res.json(produtoAtualizado)
+  }
+  catch(err){
+    res.status(err.id).json(err)
+  }
 })
 
+//Deletar
 app.delete('/produtos/:id', (req, res) => {
-  const id = req.params.id;
-  res.json({msg:"Deletando produto com id "+id});
+  const id = +req.params.id;
+  try {
+    res.json(produtoService.deletar(id));
+  } catch(err) {
+    res.status(err.id).json(err)
+  }
 })
 
 app.listen(PORT, () => {
